43ce7b191289fb1bd28cc5bd4bddcccb9908c35e
Hugo Cea <00238219@uca.edu.sv>
2020-04-17 12:57:28 -0600

    Add: Clase empleado

---
+++ src/com/HACL/x00238219/Empleado.java
@@ -0,0 +1,29 @@
+package com.HACL.x00238219;
+
+public abstract class Empleado {
+    protected String nombre;
+    protected String puesto;
+    protected double salario;
+
+    public Empleado(String nombre, String puesto, double salario) {
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
